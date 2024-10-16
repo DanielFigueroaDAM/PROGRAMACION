0519228aa4cf8d99a973ebddcb47f0a38f0482ce
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-10-16 14:45:28 +0200

    con algunos comentarios

--- probandoswitch/src/main/java/org/example/Main.java
+++ probandoswitch/src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
-
+/**
+ * Muestra la calificacion escrita de cada alumno introduciendo la nota
+ * @version 1.0
+ * @autor Daniel Figueroa Vidal
+ */
 public class Main {
     public static void main(String[] args) {
+        //
         Scanner teclado=new Scanner (System.in);
         int nota;
-        System.out.println("Dime la nota del alumno");
-        nota=teclado.nextInt();
+        System.out.println("Dime la nota del alumno"); //Pedimos las notas de un alumno
+        nota=teclado.nextInt();//Usamos el escaner para que el usuario pueda escribir las notas de los alumnos
         switch (nota) {
-            case 0,1,2,3,4:
+            case 0,1,2,3,4: //El primer case para las notas suspendidas.
                 System.out.println("Suspenso");
                 break;
             case 5:
