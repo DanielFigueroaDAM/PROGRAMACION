fe9a040384617a14ae9d4bd63958f7dbb1bd60b6
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-10-16 09:02:29 +0200

    boletinextra2ejercicios

---
+++ boletin_extra/src/main/java/org/example/boletin_extra_ej2.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class boletin_extra_ej2 {
+    public static void main(String[] args) {
+        Scanner teclado= new Scanner(System.in);
+        System.out.println("Dime los grados celsius ");
+        double gradosc = teclado.nextDouble();
+        double gradosk=gradosc +273;
+        double gradosf=((9/5)*gradosc)+32;
+        System.out.println("Los grados kelvin"+gradosk);
+        System.out.println("Los grados fahrenhait"+gradosf+"f");
+
+    }
+}
