1755add6e976346b800e0a1782802ad25fb0cb8d
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-10-24 14:44:04 +0200

    primer y segundo ejercicio

---
+++ Boletin5/src/main/java/org/example/Boletin5_ej2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class Boletin5_ej2 {
+    public static void main(String []args){
+        double suma=10, producto=10;
+        for(int i=11;i<=90;i++){
+            suma=suma+i;
+            producto=producto*i;
+        }
+        System.out.println("La suma es "+suma);
+        System.out.println("La multiplicacion es "+producto);
+    }
+}
