b01f55a9f01722cb0d4bed7d4a33f21ae24b3534
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-10-26 10:48:52 +0200

    Ejercicio 7 con dos triangulos

--- Boletin5/src/main/java/org/example/Boletin5_ej7.java
+++ Boletin5/src/main/java/org/example/Boletin5_ej7.java
@@ -25,3 +25,29 @@ public class Boletin5_ej7 {
     }
 
 }
+/*
+package org.example;
+
+import java.util.Scanner;
+
+
+public class Boletin5_ej7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un número n: ");
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for (int j=0;j<=n-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+            for(int k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+        }
+
+        scanner.close();
+    }
+
+}
