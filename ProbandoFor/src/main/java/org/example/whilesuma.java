9ecdde925001c83a24ab389c4f80cd22993c8052
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-10-28 14:09:47 +0100

    while suma comentado

--- ProbandoFor/src/main/java/org/example/whilesuma.java
+++ ProbandoFor/src/main/java/org/example/whilesuma.java
@@ -1,4 +1,5 @@
-package org.example;
+```java
+        package org.example;
 
 import java.util.Scanner;
 
@@ -6,15 +7,18 @@ public class whilesuma {
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         int n, sum=0, x=0;
+
+        // Bucle que pide números al usuario, finaliza si se introduce 999 o si se llega a 10 números
         do{
             System.out.println("Dime cada numero(para salir ingresa el 999): ");
             n = sc.nextInt();
-            if(n!=999) {
+            if(n!=999) { // Si el número no es 999, se suma al total
                 sum = sum + n;
             }
-            x++;
-        }while (n!=999 && x<10);
-        System.out.println("La suma es "+sum);
+            x++; // Aumenta el contador de números ingresados
+        }while (n!=999 && x<10); // Condición para salir del bucle
+        System.out.println("La suma es "+sum); // Muestra el total de la suma
         sc.close();
     }
 }
+```
