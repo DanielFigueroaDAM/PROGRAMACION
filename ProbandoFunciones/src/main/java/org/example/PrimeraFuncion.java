02e801169fa6dc8ef3553e4c1dd068091316e126
Figueroa <dfigueroavidal@danielcastelao.org>
2024-10-30 14:21:56 +0100

    comentando funciones

--- ProbandoFunciones/src/main/java/org/example/PrimeraFuncion.java
+++ ProbandoFunciones/src/main/java/org/example/PrimeraFuncion.java
@@ -1,29 +1,49 @@
 package org.example;
 
 import java.util.Scanner;
-
+/**
+ * Numeros comprendidos entre dos numeros
+ * @version 1.0
+ * @autor Daniel Figueroa Vidal
+ */
 
 public class PrimeraFuncion {
     public static void main(String[] args){
-        Scanner teclado1 = new Scanner(System.in);
-        int nu1 = num1(teclado1);
-        int nu2 = num2(teclado1);
-        System.out.println(" ");
-        System.out.println(" ");
+        Scanner teclado = new Scanner(System.in);
+        int nu1 = num1(teclado);
+        int nu2 = num2(teclado);
         numeros(nu1,nu2);
 
     }
-    static int num1(Scanner teclado1){
-        Scanner teclado = new Scanner(System.in);
+
+    /**
+     * Recoge un numero por teclado
+     * @since v1.0
+     * @param teclado Es el nombre del escaner
+     * @return el numero recodido
+     */
+    static int num1(Scanner teclado){
         System.out.println("Dime el primer numero ");
         return teclado.nextInt();
     }
-    static int num2(Scanner teclado1){
-        Scanner teclado = new Scanner(System.in);
+
+    /**
+     * Recoge un numero por teclado
+     * @since v1.0
+     * @param teclado Es el nombre del escaner
+     * @return el numero recodido
+     */
+    static int num2(Scanner teclado){
         System.out.println("Dime el segundo numero ");
         return teclado.nextInt();
 
     }
+
+    /**
+     *bucle para mostrar los comprendidos entre ellos
+     * @param nume1 el primer numero escrito
+     * @param nume2 el segundo numero escrito
+     */
     static void numeros(int nume1, int nume2){
         if(nume1<nume2){
             for(int i=nume1+1;nume1<nume2;nume1++) {
