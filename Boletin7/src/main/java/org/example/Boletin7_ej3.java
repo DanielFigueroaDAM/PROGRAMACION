488e8324f04a3553418fb7f987c2103a7de2e836
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-11-22 20:32:12 +0100

    Boletin 7 con ejercicio 3 terminado

--- Boletin7/src/main/java/org/example/Boletin7_ej3.java
+++ Boletin7/src/main/java/org/example/Boletin7_ej3.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Boletin7_ej3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[] notas = new int[30];
         int media = 0, alta = 0, suspensos = 0, aprobados = 0;
         String[] nombres = {
@@ -14,13 +16,14 @@ public class Boletin7_ej3 {
                 "David", "Sara", "Juan", "Clara", "Alejandro",
                 "Julia", "Pablo", "Victoria", "Héctor", "Irene"
         };
-
+        System.out.println("-----------------------------------------");
         // Generar notas aleatorias y calcular estadísticas
         for (int i = 0; i < notas.length; i++) {
-            notas[i] = (int) (Math.random() * 10);
+            notas[i] = (int) (Math.random() * 11);
             media += notas[i];
             if (notas[i] >= 5) {
                 aprobados++;
+                System.out.println(nombres[i]+" "+notas[i]);
             } else {
                 suspensos++;
             }
@@ -28,7 +31,7 @@ public class Boletin7_ej3 {
                 alta = notas[i];
             }
         }
-
+        System.out.println("-----------------------------------------");
         // Calcular media
         media /= notas.length;
 
@@ -58,6 +61,21 @@ public class Boletin7_ej3 {
         System.out.println("El número de aprobados es " + aprobados);
         System.out.println("La media es " + media);
         System.out.println("La nota más alta es " + alta);
+        String alumno;
+        System.out.print("Dime el nombre del alumno ");
+        alumno= sc.next();
+        boolean comp=true;
+        for(int i=0;i<nombres.length;i++){
+            if(alumno.equals(nombres[i])){
+                System.out.println("La nota de "+nombres[i]+" es "+notas[i]);
+                comp=false;
+            }
+            if(i==nombres.length-1 && comp==true){
+                System.out.println("El nombre no está en la lista");
+            }
+        }
+
+        sc.close();
     }
 }
 
