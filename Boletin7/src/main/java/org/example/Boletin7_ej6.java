25afb83d074adf83272b8a80127efe9edcdff259
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-11-27 22:27:22 +0100

    boletin 7 ejercicio 4 terminado

--- Boletin7/src/main/java/org/example/Boletin7_ej6.java
+++ Boletin7/src/main/java/org/example/Boletin7_ej6.java
@@ -9,37 +9,34 @@ public class Boletin7_ej6 {
                 System.out.print(numeros[i]+"-");
             }
             System.out.println("\n");
-            int indice=array(numeros,600);
+            int[] indice=array(numeros,600);
             System.out.println("\n"+indice);
 
         }
-        static int array(int[] lista, int numero){
+        static int[] array(int[] lista, int numero){
             int mitad = lista.length / 2;
-            int j=0;
+            int j=0, y=0;
             int[] lista2 = new int[mitad];
                 if(numero> mitad){
                     for(int i = mitad; i < lista.length-1; i++){
                         j++;
-                        lista[i] = lista2[j];
+                        lista2[j] = lista[i];
                     }
                     return array(lista2,numero);
 
                 }
                 else if(numero<mitad){
-                    for(int i = 0; i < mitad-1; i++){
-                        j++;
-                        lista[i] = lista2[j];
+                    for(int i = 0; i < mitad; i++){
+                        y++;
+                        lista2[y] = lista[i];
                     }
                     return array(lista2,numero);
 
                 }
                 else if(numero==mitad){
-                    return -2;
+                    System.out.println(mitad);
                 }
-                else {
-                    return -1;
-                }
-
+                    return new int[]{-1};
 
         }
 }
--- Boletin7/src/main/java/org/example/Boletin8_ej9.java
+++ Boletin7/src/main/java/org/example/Boletin8_ej9.java
@@ -6,31 +6,62 @@ import static java.util.Arrays.binarySearch;
 public class Boletin8_ej9 {
     public static void main(String[] args) {
         int[] numeros = {10, 20, 20, 40, 40, 50, 60};
-        int[] vuelta=array1(numeros);
-        for(int numero:vuelta) {
+        int[] resultado = eliminarDuplicados(numeros);
+
+        // Imprimir el arreglo resultante
+        for (int numero : resultado) {
             System.out.println(numero);
         }
-
     }
-    /*
-    static int[] array(int[] lista){
-        int [] temporal = new int [0];
-        int[] taboa;
-        for(int elemento : taboa){
-            if(binarySearch (temporal,elemento==-1) {
-                int[] novo = new int[temporal.lenght + 1];
-                for (int i = 0; i < temporal.lenght; i++) {
-                    novo[i] = temporal[i];
-                }
-                novo[temporal.length] = elemento;
-                temporal = novo;
+
+    static int[] eliminarDuplicados(int[] lista) {
+        int[] temporal = new int[lista.length]; // Arreglo temporal para almacenar únicos
+        int contador = 0; // Contador para elementos únicos
+
+        for (int i = 0; i < lista.length; i++) {
+            if (!existeEnArreglo(temporal, contador, lista[i])) {
+                temporal[contador] = lista[i];
+                contador++;
             }
         }
 
-        return temporal;
+        // Crear un nuevo arreglo con el tamaño exacto de elementos únicos
+        int[] resultado = new int[contador];
+        for (int i = 0; i < contador; i++) {
+            resultado[i] = temporal[i];
+        }
+
+        return resultado;
     }
-    */
-    
+
+    // Método auxiliar para verificar si un número ya existe en el arreglo
+    static boolean existeEnArreglo(int[] arreglo, int tamaño, int numero) {
+        for (int i = 0; i < tamaño; i++) {
+            if (arreglo[i] == numero) {
+                return true; // El número ya existe en el arreglo
+            }
+        }
+        return false; // El número no existe en el arreglo
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     static int[] array1(int[] lista){
         int restante=0;
         int[] list = new int[lista.length];
@@ -41,7 +72,6 @@ public class Boletin8_ej9 {
             for (int j=i+1;j<list.length;j++) {
                 if (list[j] == lista[i]) {
                     restante++;
-
                 }
             }
         }
@@ -66,3 +96,4 @@ public class Boletin8_ej9 {
         return result;
     }
 }
+*/
