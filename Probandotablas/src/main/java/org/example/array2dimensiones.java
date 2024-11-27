f1cc5d9b5501d14df04df712737e04179fc975e8
Figueroa <dfigueroavidal@danielcastelao.org>
2024-11-27 14:42:45 +0100

    arrays dimensionales 2 d

--- Probandotablas/src/main/java/org/example/array2dimensiones.java
+++ Probandotablas/src/main/java/org/example/array2dimensiones.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class array2dimensiones {
     public static void main(String[] args) {
+        int [][]dosd;
+        dosd =new int[][]{
+            {1,1,0,0,0},
+            {1,1,0,0,0},
+            {1,1,0,0,0},
+            {1,1,0,0,0},
+            {1,1,0,0,0},
+        };
+        int j=0;
+        for(int i=0;i<5;i++){
+            for(;j<5;j++){
+                System.out.println(dosd[i][j]);
+            }
+            j=0;
+        }
 
     }
 }
