58b6e0b123ea2d022423282cf9cb0b280bf49c0b
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-12-26 21:28:55 +0100

    ejercicio 7 boletin 8 terminado

---
+++ Boletin8/src/main/java/org/example/Boletin8_ej7.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Boletin8_ej7 {
+    public static void main(String[] args) {
+        String text="javeros";
+        String textMayus = text.toUpperCase();
+        System.out.println(textMayus);
+        String newMinusculas = textMayus.toLowerCase();
+        System.out.println(newMinusculas);
+    }
+}
