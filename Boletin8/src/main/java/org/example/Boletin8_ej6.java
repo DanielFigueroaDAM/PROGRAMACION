d025f4748ae421c206a666d1cf4e7a1d6a2b1334
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-12-26 21:15:48 +0100

    ejercicio 6 boletin 8 terminado

--- Boletin8/src/main/java/org/example/Boletin8_ej6.java
+++ Boletin8/src/main/java/org/example/Boletin8_ej6.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Boletin8_ej6 {
     public static void main(String[] args) {
-
+        String text="www. javadende0. com";
+        int indiceText = text.indexOf('d');
+        String copiedStartText = text.substring(0, indiceText);
+        String copiedEndText = text.substring(indiceText, text.length());
+        String newText = copiedStartText + copiedEndText;
+        System.out.println(newText);
     }
 }
