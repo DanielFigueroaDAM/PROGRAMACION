418e376146666f6c117f2aea80f4439df125ff2e
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-12-26 22:00:34 +0100

    ejercicio 8 boletin 8 terminado

---
+++ Boletin8/src/main/java/org/example/Boletin8_ej8.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Boletin8_ej8 {
+    public static void main(String[] args) {
+        String txt1="Java";
+        String txt2="JavaScript";
+        if(txt1.equals(txt2)){
+            System.out.println("Son iguales");
+        }
+        else {
+            System.out.println("No son iguales");
+        }
+    }
+}
