f8ea2f5651dddc70e2756cdcd58e03c20101bc0b
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-12-26 22:05:58 +0100

    ejercicio 9 boletin 8 terminado

---
+++ Boletin8/src/main/java/org/example/Boletin8_ej9.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Boletin8_ej9 {
+    public static void main(String[] args) {
+        String text1="Jeve jeve jeve";
+        String text2=text1.replaceAll("e", "a");
+        System.out.println(text2);
+    }
+}
