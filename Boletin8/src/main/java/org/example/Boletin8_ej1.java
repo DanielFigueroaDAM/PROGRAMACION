49204a24638cf68b1e5a00d1c03f42d8d8fec74d
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-12-30 22:38:57 +0100

    mas comentarios

--- Boletin8/src/main/java/org/example/Boletin8_ej1.java
+++ Boletin8/src/main/java/org/example/Boletin8_ej1.java
@@ -1,8 +1,21 @@
 package org.example;
 
+/**
+ * Programa que calcula la longitud de una cadena de texto.
+ *
+ * Funcionalidad:
+ * - Almacena una cadena de texto.
+ * - Calcula y muestra la longitud de la cadena.
+ *
+ * @version 1.0
+ * @autor Daniel Figueroa Vidal
+ */
 public class Boletin8_ej1 {
     public static void main(String[] args) {
-        String texto= "Isto é Java!";
+        // Declara una variable String con el valor "Isto é Java!"
+        String texto = "Isto é Java!";
+
+        // Imprime la longitud de la cadena de texto
         System.out.println(texto.length());
     }
 }
