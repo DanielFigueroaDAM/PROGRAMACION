cd70711d94a8ff86262b40ed324f22030ffc70e9
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-01-11 10:55:04 +0100

    boletin 8 ejercicio 6 y 7 terminados

--- Boletin8.2/src/main/java/org/example/Boletin8_2_ej6.java
+++ Boletin8.2/src/main/java/org/example/Boletin8_2_ej6.java
@@ -1,5 +1,81 @@
 package org.example;
 
 public class Boletin8_2_ej6 {
+    public static void main(String[] args) {
+        String cadea="algoritmo";
+        System.out.println(consonantes(cadea));
+        System.out.println(vocales(cadea));
+        System.out.println(siguienteVocal(cadea));
+        System.out.println(palindromo("hola"));
+    }
+    public static String consonantes(String cadea){
+        String consonantes="";
+        for(int i=0;i<cadea.length();i++){
+            if(cadea.charAt(i)!='a' && cadea.charAt(i)!='e' && cadea.charAt(i)!='i' && cadea.charAt(i)!='o' && cadea.charAt(i)!='u'){
+                consonantes=consonantes+cadea.charAt(i);
+            }
+        }
+        return consonantes;
 
+    }
+    public static String vocales(String cadea){
+        String vocales="";
+        for(int i=0;i<cadea.length();i++){
+            if(cadea.charAt(i)=='a' || cadea.charAt(i)=='e' || cadea.charAt(i)=='i' || cadea.charAt(i)=='o' || cadea.charAt(i)=='u'){
+                vocales=vocales+cadea.charAt(i);
+            }
+        }
+        return vocales;
+
+    }
+    public static String siguienteVocal(String cadea){
+        String siguienteVocal="";
+        for(int i=0;i<cadea.length();i++){
+            if(cadea.charAt(i)=='a'){
+                siguienteVocal=siguienteVocal+'e';
+            }
+            else if(cadea.charAt(i)=='e'){
+                siguienteVocal=siguienteVocal+'i';
+            }
+            else if(cadea.charAt(i)=='i'){
+                siguienteVocal=siguienteVocal+'o';
+            }
+            else if(cadea.charAt(i)=='o'){
+                siguienteVocal=siguienteVocal+'u';
+            }
+            else if(cadea.charAt(i)=='u'){
+                siguienteVocal=siguienteVocal+'a';
+            }
+            else{
+                siguienteVocal=siguienteVocal+cadea.charAt(i);
+            }
+        }
+        return siguienteVocal;
+    }
+    public static boolean palindromo(String cadea){
+        boolean palindromo=true;
+        String minus=cadea.toLowerCase();
+        String limpia = minus.replaceAll(" ","");
+        String limpia2="";
+        char sub=0;
+        for(int i=0;i<limpia.length();i++){
+            sub=switch (limpia.charAt(i)) {
+                case 'á' -> 'a';
+                case 'é' -> 'e';
+                case 'í' -> 'i';
+                case 'ó' -> 'o';
+                case 'ú' -> 'u';
+                default -> limpia.charAt(i);
+            };
+            limpia2+=sub;
+        }
+        String inversa="";
+        for(int i = limpia2.length()-1; i>=0;i--){
+            inversa+=limpia2.charAt(i);
+        }
+        if(!limpia2.equals(inversa)){
+            palindromo=false;
+        }
+        return palindromo;
+    }
 }
---
+++ Boletin8.2/src/main/java/org/example/Boletin8_2_ej7.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Boletin8_2_ej7 {
+    public static void main(String[] args) {
+        String text1 ="scadeas";
+        String text2 ="subcadea";
+        System.out.println(estaContido(text1,text2));
+        System.out.println(ordenAlfabetico(text1,text2));
+    }
+    public static boolean estaContido(String text1, String text2){
+        if(text1.contains(text2) || text2.contains(text1)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static String ordenAlfabetico(String text1, String text2){
+        for(int i=0;i<text1.length();i++) {
+            if (text1.charAt(i) < text2.charAt(i)) {
+                return text1;
+            } else if (text1.charAt(i) > text2.charAt(i)) {
+                return text2;
+            }
+            else if(text1.length()>text2.length()){
+                return text2;
+            }
+            else if(text1.length()<text2.length()){
+                return text1;
+            }
+        }
+        return "iguales";
+    }
+}
