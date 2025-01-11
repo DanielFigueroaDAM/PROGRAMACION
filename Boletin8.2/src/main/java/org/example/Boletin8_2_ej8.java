371b5e0fa29452409d31cf466050d744caa3bc65
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-01-11 11:01:19 +0100

    boletin 8 ejercicio 8 terminados

--- Boletin8.2/src/main/java/org/example/Boletin8_2_ej8.java
+++ Boletin8.2/src/main/java/org/example/Boletin8_2_ej8.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Boletin8_2_ej8 {
     public static void main(String[] args) {
-
+    String binario="1010";
+    System.out.println(binarioADecimal(binario));
+    }
+    public static int binarioADecimal(String binario){
+        int decimal=0;
+        for(int i=0;i<binario.length();i++){
+            if(binario.charAt(i)=='1'){
+                decimal=decimal+(int)Math.pow(2,binario.length()-1-i);
+            }
+            else if(binario.charAt(i)!='0'){
+                return -1;
+            }
+        }
+        return decimal;
     }
 }
