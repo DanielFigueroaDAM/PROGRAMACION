e4273ee113b869c7f298dd1801a5e118538e81fa
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-01-18 12:46:24 +0100

    ejercio 12 terminados

---
+++ Boletin8.2/src/main/java/org/example/Boletin8_2_ej12.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Boletin8_2_ej12 {
+    public static void main(String[] args) {
+        String cadea = "Holassssssssssssssssssssssssssssssssss holasss hol Holasassssssssss";
+        analizador(cadea);
+    }
+
+    public static void analizador(String cadea) {
+        //pasar a array
+        String[] palabras = cadea.split(" ");
+        //contar palabras
+        System.out.println("Numero de palabras: " + palabras.length);
+        //contar letras y palabra mas larga
+        int contadorLetras = 0;
+        String palabraMasLarga = "", aux = "";
+        for (int i = 0; i < cadea.length(); i++) {
+            if (cadea.charAt(i) != ' ') {
+                contadorLetras++;
+            }
+        }
+        for (int i = 0; i < palabras.length; i++) {
+            if (palabras[i].length() > palabraMasLarga.length()) {
+                palabraMasLarga = palabras[i];
+            }
+        }
+        System.out.println("Numero de letras: " + contadorLetras);
+        System.out.println("Palabra mas larga: " + palabraMasLarga);
+    }
+}
