0cbc6c5b862d22e7f8aaf4ba5fe890fea63221b4
Figueroa <dfigueroavidal@danielcastelao.org>
2025-01-27 14:15:20 +0100

    puf

--- Boletin8.3/src/main/java/org/example/Ejercicio1_8_3.java
+++ Boletin8.3/src/main/java/org/example/Ejercicio1_8_3.java
@@ -2,14 +2,13 @@ package org.example;
 
 public class Ejercicio1_8_3 {
     public static void main(String[] args) {
-        String palabra="figorifico";
-        int secuencia=4;
+        String palabra="casa";
+        int secuencia=2;
         String [] array = new String[palabra.length()-secuencia+1];
 
         for(int i=0;i<=palabra.length()-secuencia;i++){
             String variable=palabra.substring(i,secuencia+i);
             array[i]=variable;
-            System.out.println(variable);
         }
         for (int i=0;i< array.length;i++){
             System.out.println(array[i]);
