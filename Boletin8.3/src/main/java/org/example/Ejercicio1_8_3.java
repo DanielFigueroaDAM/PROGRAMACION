740f5f6ee22aa016ffc764f49a0214b836eefeb8
Figueroa <dfigueroavidal@danielcastelao.org>
2025-01-27 14:38:08 +0100

    funcional

--- Boletin8.3/src/main/java/org/example/Ejercicio1_8_3.java
+++ Boletin8.3/src/main/java/org/example/Ejercicio1_8_3.java
@@ -5,7 +5,10 @@ public class Ejercicio1_8_3 {
         String palabra="casa";
         int secuencia=2;
         String [] array = new String[palabra.length()-secuencia+1];
+        sec(palabra,secuencia,array);
 
+    }
+    public static void sec(String palabra, int secuencia,String[] array){
         for(int i=0;i<=palabra.length()-secuencia;i++){
             String variable=palabra.substring(i,secuencia+i);
             array[i]=variable;
