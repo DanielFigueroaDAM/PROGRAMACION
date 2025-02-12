d21fe2fb8bee7f1ac3ac6b9e54e1490f8e0ca38a
Figueroa <dfigueroavidal@danielcastelao.org>
2025-02-12 10:48:29 +0100

    objeto calendario

---
+++ ProbandoObjetos/src/main/java/paracalendario/AppCalendario.java
@@ -0,0 +1,16 @@
+package paracalendario;
+
+public class AppCalendario {
+    public static void main(String[] args) {
+        Calendario c1 = new Calendario(28,2,2020);
+        c1.mostrarCalendario();
+        c1.incrementarDia();
+        c1.mostrarCalendario();
+        c1.incrementarMes();
+        c1.mostrarCalendario();
+        c1.incrementarAno();
+        c1.mostrarCalendario();
+        c1.incrementarDia();
+        c1.mostrarCalendario();
+    }
+}
