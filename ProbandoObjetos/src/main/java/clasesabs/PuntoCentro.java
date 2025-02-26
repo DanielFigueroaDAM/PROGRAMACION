d1755c9d6c255c83b8669014df4a8c03c13601fc
Figueroa <dfigueroavidal@danielcastelao.org>
2025-02-26 10:42:10 +0100

    clases abstractas

---
+++ ProbandoObjetos/src/main/java/clasesabs/PuntoCentro.java
@@ -0,0 +1,10 @@
+package clasesabs;
+
+public class PuntoCentro extends PuntoBase{
+    public PuntoCentro() {
+        super();
+    }
+    public String aCadea(){
+        return ("el punto eso"+getX()+getY());
+    }
+}
