2bac447709aa3e1bd2eab4c90cb5f701c4031fc7
Figueroa <dfigueroavidal@danielcastelao.org>
2025-02-27 14:14:16 +0100

    mas canbios

--- ProbandoObjetos/src/main/java/clasesabs/Solido.java
+++ ProbandoObjetos/src/main/java/clasesabs/Solido.java
@@ -4,7 +4,7 @@ public abstract class Solido extends FiguraXeometrica{
     public Solido(){
         super();
     }
-    public Solido(double x, double y;){
+    public Solido(double x, double y){
         super(x,y);
     }
 }
