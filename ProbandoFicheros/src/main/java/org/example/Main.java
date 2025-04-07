a6728a49466f3316aaf089dca7fadbb62bf2cd17
Figueroa <dfigueroavidal@danielcastelao.org>
2025-04-07 13:02:04 +0200

    añadido el main

---
+++ ProbandoFicheros/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String texto = "";
+        FileReader entrada = null;
+
+        try {
+            entrada = new FileReader("/home/dam/PROGRAMACION/ProbandoFicheros/src/main/java/org/example/ficheiroTextoProba");
+            int caracter = entrada.read();
+
+            while (caracter != -1) {
+                texto += (char) caracter;
+                caracter = entrada.read();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro de entrada/saida: " + e.getMessage());
+        } finally {
+            if (entrada != null) {
+                try {
+                    entrada.close();
+                } catch (IOException e) {
+                    System.out.println("Erro de entrada/saida al cerrar: " + e.getMessage());
+                }
+            }
+        }
+
+        System.out.println("O texto do ficheiro é: " + texto);
+    }
+}
