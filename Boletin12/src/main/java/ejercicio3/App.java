cfb7907431402e31a362efc6b131b2982ea0a740
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-04-20 02:21:00 +0200

    avanzando ejercicio 3 del boletin 12

---
+++ Boletin12/src/main/java/ejercicio3/App.java
@@ -0,0 +1,14 @@
+package ejercicio3;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Introduce o nome do ficheiro");
+        String nomeFicheiro = tec.nextLine();
+        LeerFicheiro f1 = new LeerFicheiro(nomeFicheiro);
+        f1.mostrarPalabras();
+
+    }
+}
---
+++ Boletin12/src/main/java/ejercicio3/LeerFicheiro.java
@@ -0,0 +1,48 @@
+package ejercicio3;
+
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeerFicheiro {
+    public String [] listaPalabras = new String[100];
+    public LeerFicheiro(String nomeFicheiro) {
+        // vamos a meter cada palabra separada por un espacio en un array
+        String texto = "";
+        FileReader lector = null;
+        int contador = 0;
+        String palabra = "";
+        try{
+            lector = new FileReader("/home/figue/PROGRAMACION/Boletin12/src/main/java/ejercicio3/"+nomeFicheiro);
+            int caracter = lector.read();
+            while (caracter != -1) {
+                if(caracter !=(char)' '){
+                    palabra += (char) caracter;
+                }else{
+                    listaPalabras[contador] = palabra;
+                    contador++;
+                    palabra = "";
+                }
+                caracter = lector.read();
+            }
+        }catch (IOException e){
+            System.out.println("Error de entrada/salida: " + e.getMessage());
+        } finally {
+            if (lector != null) {
+                try {
+                    lector.close();
+                } catch (IOException e) {
+                    System.out.println("Error de entrada/salida al cerrar: " + e.getMessage());
+                }
+            }
+        }
+    }
+    public void mostrarPalabras() {
+        for (int i = 0; i < listaPalabras.length; i++) {
+            if (listaPalabras[i] != null) {
+                System.out.println(listaPalabras[i]);
+            }
+        }
+    }
+}
