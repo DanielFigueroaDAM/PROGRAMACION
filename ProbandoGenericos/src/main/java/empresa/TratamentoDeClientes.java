d99f3a38e10afa19125122e3804ec267ed490db9
Figueroa <dfigueroavidal@danielcastelao.org>
2025-04-24 14:45:53 +0200

    mas arrayList

--- ProbandoGenericos/src/main/java/empresa/TratamentoDeClientes.java
+++ ProbandoGenericos/src/main/java/empresa/TratamentoDeClientes.java
@@ -2,6 +2,7 @@ package empresa;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class TratamentoDeClientes {
     public static void main(String args[]){
@@ -12,7 +13,14 @@ public class TratamentoDeClientes {
         coleccionCliente.add(new Cliente("123456787H","Maria","26/09/2021"));
         Cliente unCliente = new Cliente("123456587H","Oscar","26/09/2021");
         coleccionCliente.add(unCliente);
+        Iterator <Cliente> indice = coleccionCliente.iterator();
+        for(;indice.hasNext();){
+            System.out.println(indice.next());
+        }
         coleccionCliente.remove(unCliente);
         coleccionCliente.clear();
+        coleccionCliente.toString();
+        Object[]clientes= coleccionCliente.toArray();
+
     }
 }
