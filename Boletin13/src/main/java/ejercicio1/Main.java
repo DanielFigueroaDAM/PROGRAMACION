3288e6f144c56804a3525738dd5c5b83ce6bf4b9
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-04-29 20:34:05 +0200

    ejercicio de concatenar elementos de 2 arrays por indice

--- Boletin13/src/main/java/ejercicio1/Main.java
+++ Boletin13/src/main/java/ejercicio1/Main.java
@@ -10,11 +10,40 @@ public class Main {
         for(Integer p : arrayNuevo ){
             System.out.println(p);
         }
+        String [] array3 = {"Hola", "Mundo"};
+        String [] array4 = {"Adios", "Mundo"};
+        String[] arrayNuevo2 = concatenarArrays(array3, array4);
+        for(String p : arrayNuevo2 ){
+            System.out.println(p);
+        }
+        String [] array5 = concatenarElementosArrays(array1, array2);
+        for(String p : array5 ){
+            System.out.println(p);
+        }
 
     }
     public static <T> T[] concatenarArrays(T[] primeiro,T[] segundo){
         T[] nuevoArray = Arrays.copyOf(primeiro,primeiro.length + segundo.length);
         System.arraycopy(segundo, 0, nuevoArray, primeiro.length, segundo.length);
+        return nuevoArray;
+    }
+
+    /**
+     * Concatenar elementos pasandolo a tipo String
+     * @param primeiro
+     * @param segundo
+     * @return
+     * @param <T>
+     */
+    public static <T> String[] concatenarElementosArrays(T[] primeiro,T[] segundo){
+        String[] nuevoArray = new String[primeiro.length];
+        if(primeiro.length!= segundo.length){
+            throw new IllegalArgumentException("Los arrays no tienen la misma longitud");
+        }else {
+            for (int i = 0; i < primeiro.length; i++) {
+                nuevoArray[i] = primeiro[i]+""+segundo[i];
+            }
+        }
         return nuevoArray;
     }
 }
