fa000e3396ccab03107731e66f632f0218b8f757
Figueroa <dfigueroavidal@danielcastelao.org>
2025-05-08 12:51:43 +0200

    avanzando en collections

--- ProbandoGenericos/src/main/java/Bol3Ex6/App.java
+++ ProbandoGenericos/src/main/java/Bol3Ex6/App.java
@@ -5,14 +5,14 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
         List<Integer> listaNumeros = new ArrayList<>();
-        for (int i=0; i<20; i++){
+        for (int i = 0; i < 20; i++) {
             int numeroRandom = (int) (Math.random() * 10) + 1;
             listaNumeros.add(numeroRandom);
         }
-        Comparator <Integer> comparador = Comparator.naturalOrder();
+        Comparator<Integer> comparador = Comparator.naturalOrder();
         listaNumeros.sort(comparador);
         System.out.println(listaNumeros);
-        Set <Integer> conjuntoNumerosDesordenado = Set.copyOf(listaNumeros);
+        Set<Integer> conjuntoNumerosDesordenado = Set.copyOf(listaNumeros);
         System.out.println(conjuntoNumerosDesordenado);
         Set<Integer> conjuntoNumerosOrdenado = new HashSet<>();
         conjuntoNumerosOrdenado.addAll(listaNumeros);
@@ -20,9 +20,10 @@ public class App {
 
 
         Set<Integer> repetidos = new HashSet<>();
-        repetidos.addAll(listaNumeros){
-
-        }
+        repetidos.addAll(listaNumeros);
 
     }
 }
+
+
+
